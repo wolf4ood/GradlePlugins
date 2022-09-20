e02612d9a4dc1a0d7d8cf4859293fbabd8e4db6e
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2022-09-20 14:03:44 +0200

    cleanup: move autodoc domain objects here

--- autodoc-model/build.gradle.kts
+++ autodoc-model/build.gradle.kts
@@ -4,8 +4,17 @@ plugins {
 
 val jupiterVersion: String by project
 val assertj: String by project
+val jetBrainsAnnotationsVersion: String by project
+val jacksonVersion: String by project
 
 dependencies {
+
+    api("org.jetbrains:annotations:${jetBrainsAnnotationsVersion}")
+    api("com.fasterxml.jackson.core:jackson-core:${jacksonVersion}")
+    api("com.fasterxml.jackson.core:jackson-annotations:${jacksonVersion}")
+    api("com.fasterxml.jackson.core:jackson-databind:${jacksonVersion}")
+    api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${jacksonVersion}")
+
     // Use JUnit test framework for unit tests
     testImplementation("org.junit.jupiter:junit-jupiter-api:${jupiterVersion}")
     testImplementation("org.junit.jupiter:junit-jupiter-params:${jupiterVersion}")
