9e534e89ecf3e32754ea96a42a044608a5b21adf
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2022-09-27 15:46:13 +0200

    feat: implement autodoc plugin (#8)
    * renamed autodoc-core -> autodoc-processor
    
    * feat: adds a gradle plugin that adds the annotation processor dep
    
    * Provide way to configure the processor version, or resolve version automatically
    
    * resolve the extension dynamically
    
    * capitalization

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "edcPlugins"
 
 include("plugins:autodoc:autodoc-plugin")
-include("plugins:autodoc:autodoc-core")
+include("plugins:autodoc:autodoc-processor")
 include("plugins:autodoc:autodoc-extension-test")
 include("plugins:autodoc:autodoc-spi-test")
 include("runtime-metamodel")
