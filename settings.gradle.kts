fa073ca62180151cc2d70e5c98b0230e47999c05
ndr_brt <andrea.bertagnolli@gmail.com>
2023-01-20 11:36:58 +0100

    refactor: format settings.gradle.kts (#74)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,10 @@
 rootProject.name = "GradlePlugins"
-include("plugins:module-names")
-include("plugins:test-summary")
-include("plugins:openapi-merger")
-include("plugins:edc-build")
-include("plugins:autodoc:autodoc-plugin")
-include("plugins:autodoc:autodoc-processor")
-include("runtime-metamodel")
-include("version-catalog")
+
+include(":plugins:autodoc:autodoc-plugin")
+include(":plugins:autodoc:autodoc-processor")
+include(":plugins:edc-build")
+include(":plugins:module-names")
+include(":plugins:openapi-merger")
+include(":plugins:test-summary")
+include(":runtime-metamodel")
+include(":version-catalog")
